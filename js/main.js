ca7936033fdec74859cbb86b2e3f2ed8dea5f627
Ruslan <ruslanhlukhov@gmail.com>
2019-03-05 16:25:38 +0200

    v2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
-    $(".item__P").magnificPopup({
+    $(".item").magnificPopup({
         type : 'image',
+       // gallery:{
+        //    enabled: true
+        //},
         removavDelay : 500,
         mainClass: 'mfp-fade'
     });
 });
-
+$(function(){ 
+    $('a[href^="#').bind('click.smoothscroll', function(){
+        var target = $(this).attr('href'),
+        bl_top = $(target).offset().top - 40;
+    $('body, html').animate({scrollTop: bl_top},800);
+    return false;
+    })
+});
